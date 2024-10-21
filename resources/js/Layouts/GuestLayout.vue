23cd65031198f02ec0da340e6413510422c5834a
abay bay <akbar25005@gmail.com>
2024-10-21 11:04:58 +0700

    update: Register Page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -8,9 +8,26 @@ export default defineComponent({
         Header,
     },
     setup() {
+        const path = window.location.pathname; // Contoh: "/register-smp"
 
+        // Pisahkan path untuk mendapatkan bagian terakhir dari URL
+        const registerType = path.split('/').pop(); // Akan menghasilkan 'r
+        console.log(registerType)
+        const isSMP = registerType === 'register-smp';
+        const isSMK = registerType === 'register-smk';
+        const schoolType = isSMP ? 'SMP' : 'SMK';
 
-        return {}
+        const logoSMK = "/images/LOGO_SMK_YATINDO.png"
+        const logoSMP = "/images/LOGO_SMP_YATINDO.png"
+
+        const validLogo = isSMK ? logoSMK : logoSMP
+
+        return {
+            isSMP,
+            isSMK,
+            schoolType,
+            validLogo
+        }
     }
 })
 </script>
@@ -23,27 +40,12 @@ export default defineComponent({
         <div class="w-full min-[700px]:w-[65%] ">
             <slot />
         </div>
-        <div
-            class="max-[700px]:hidden w-[28%] fixed top-0 right-0  bg-no-repeat bg-top	bg-cover		 text-white h-[100vh] flex flex-col justify-center items-center" style="background-image: url(/images/bg-auth.png);">
+        <div class="max-[700px]:hidden w-[28%] fixed top-0 right-0  bg-no-repeat bg-top	bg-cover		 text-white h-[100vh] flex flex-col justify-center items-center"
+            style="background-image: url(/images/bg-auth.png);">
             <div class="px-4 py-2 bg-white font-bold text-black rounded-full ">PPDB YATINDO</div>
-            <img src="/images/LOGO_SMK_YATINDO.png" width="150" class="my-10" />
-            <h1 class="font-bold text-xl text-center">SMK <br/>TINTA EMAS INDONESIA</h1>
+            <img :src="validLogo" width="150" class="my-10" />
+            <h1 class="font-bold text-xl text-center">{{ schoolType }} <br />TINTA EMAS INDONESIA</h1>
         </div>
-        <!-- Kolom kanan untuk gambar (disembunyikan di mobile) -->
-        <!-- Tambahkan elemen teks di atas gambar dan pusatkan -->
-        <!-- <div class="d-none d-md-flex flex-column w-50 h-100 position-relative">
-                    <div class="h-100 d-flex flex-row justify-content-end align-items-center">
-                        <n-image width="500" src="/images/bg-auth.png" preview-disabled style="object-fit: cover;" />
-                        <div
-                            class="position-absolute top-50 start-50 translate-middle text-white text-center d-flex flex-column align-items-center">
-                            <n-image src="/images/logo-yatindo-hd.png" width="150" class="mb-2" />
-                            <div class="d-flex flex-column align-items-center">
-                                <h2 class="mb-3">SMK-SMP</h2>
-                                <h3>TINTA EMAS INDONESIA</h3>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
     </n-notification-provider>
 </template>
 
